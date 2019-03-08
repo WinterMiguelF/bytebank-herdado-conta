bdb903356e038f12c2e510d50d1ff0fb1865870b
WinterMiguelF <winterucb@gmail.com>
2019-03-08 12:46:49 -0300

    first commit

---
+++ src/br/com/bytebank/banco/especial/ContaEspecial.java
@@ -0,0 +1,15 @@
+package br.com.bytebank.banco.especial;
+
+public class ContaEspecial extends Conta {
+
+	public ContaEspecial(int agencia, int numero) {
+		super(agencia, numero);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		// TODO auto-generated method stub
+
+	}
+
+}
