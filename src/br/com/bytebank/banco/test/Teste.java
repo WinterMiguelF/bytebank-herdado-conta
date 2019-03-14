abdc63f883aa1e71d71eb1b884f49edbcc29650b
WinterMiguelF <winterucb@gmail.com>
2019-03-14 09:50:08 -0300

    Finalização curso java parte 5

--- src/br/com/bytebank/banco/test/Teste.java
+++ src/br/com/bytebank/banco/test/Teste.java
@@ -1,5 +1,7 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
@@ -7,17 +9,18 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(false);
+		// System.out.println("x");
+		// System.out.println(3);
+		// System.out.println(false);
 
-		ContaCorrente cc = new ContaCorrente(22, 33);
-		ContaPoupanca cp = new ContaPoupanca(33, 22);
+		Object cc = new ContaCorrente(22, 33);
+		Object cp = new ContaPoupanca(33, 22);
+		Object cliente = new Cliente();
 
 		System.out.println(cc);
 		System.out.println(cp);
 
-		println(cc);
+		// println(cliente);
 
 	}
 
@@ -30,7 +33,7 @@ public class Teste {
 	static void println(boolean valor) {
 	}
 
-	static void println(ContaCorrente conta) {
+	static void println(Object referencia) {
 	}
 
 }
